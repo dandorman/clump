8b59d75ab6aace6ff09d01a8d99b1863a68afed5
Dan Dorman <dan@dorman.us>
2026-01-24 19:18:12 -0700

    Start modernization
    - move from Om -> Reagent
    - move from whatever the hell (cljsbuild?) to shadow-cljs
    - move from Leinengen -> deps.edn

--- src/clump/core.cljs
+++ src/clump/core.cljs
@@ -1,24 +1,21 @@
 (ns clump.core
-  (:require-macros [cljs.core.async.macros :as m :refer [go]])
   (:require [clump.game :as game]
             [clump.ui.utils :refer [alert class-names]]
-            [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]
-            [cljs.core.async :refer [chan put! <!]]))
+            [reagent.core :as r]
+            [reagent.dom :as rdom]))
 
-(enable-console-print!)
+(defonce game-state (r/atom {}))
 
-(def game-state (atom {}))
+(defonce game-history (r/atom []))
 
-(def game-history (atom []))
 (add-watch game-state :history
            (fn [_ _ _ n]
              (when-not (= (last @game-history) n)
                (swap! game-history conj n))))
 
 (defn new-game! []
-  (swap! game-history empty)
-  (swap! game-state #(merge % (game/new-game)))
+  (reset! game-history [])
+  (swap! game-state merge (game/new-game))
   game-state)
 
 (defn undo! []
@@ -26,95 +23,89 @@
     (swap! game-history pop)
     (reset! game-state (last @game-history))))
 
-(defn no-clumps! [game]
-  (if-let [clump (first (game/clumps (:board @game)))]
+(defn no-clumps! []
+  (if-let [clump (first (game/clumps (:board @game-state)))]
     (do
-      (om/transact! game :score dec)
+      (swap! game-state update :score dec)
       (alert "Sorry, but there IS a clump."))
-    (let [drawn (game/draw @game)]
-      (om/update! game (assoc (dissoc drawn :draw) :board (concat (:board drawn) (:draw drawn)))))))
+    (let [drawn (game/draw @game-state)]
+      (swap! game-state #(assoc (dissoc drawn :draw) :board (concat (:board drawn) (:draw drawn)))))))
 
-(defn hint! [g]
-  (if-let [clump (game/hint @g)]
-    (om/transact! g :board (fn [_] (map #(if ((set clump) %) (assoc % :highlight true) %) (:board @g))))
+(defn hint! []
+  (if-let [clump (game/hint @game-state)]
+    (swap! game-state update :board
+           (fn [board] (map #(if ((set clump) %) (assoc % :highlight true) %) board)))
     (alert "No clumps?")))
 
+(defn card-selected! [card]
+  (swap! game-state #(-> (game/card-selected % card)
+                         (update :board (fn [board] (map (fn [c] (dissoc c :highlight)) board)))))
+  (when (game/over? @game-state)
+    (alert "We're done here.")))
+
 (defn shape-wrapper [contents]
-  (dom/svg #js {:viewBox "0 0 100 100"}
-           contents))
+  [:svg {:view-box "0 0 100 100"}
+   contents])
 
 (defmulti shape :shape)
 
 (defmethod shape :circle [{:keys [color fill]}]
-  (shape-wrapper (dom/circle #js {:className (class-names [color fill])
-                                  :cx 50 :cy 50 :r 45})))
+  [shape-wrapper
+   [:circle {:class (class-names [color fill])
+             :cx 50 :cy 50 :r 45}]])
 
 (defmethod shape :square [{:keys [color fill]}]
-  (shape-wrapper (dom/rect #js {:className (class-names [color fill])
-                                :x 5 :y 5 :width 90 :height 90})))
+  [shape-wrapper
+   [:rect {:class (class-names [color fill])
+           :x 5 :y 5 :width 90 :height 90}]])
 
 (defmethod shape :triangle [{:keys [color fill]}]
-  (shape-wrapper (dom/polygon #js {:className (class-names [color fill])
-                                   :points "10 85, 50 16, 90 85"})))
+  [shape-wrapper
+   [:polygon {:class (class-names [color fill])
+              :points "10 85, 50 16, 90 85"}]])
 
 (defmethod shape :blank [_]
-  (shape-wrapper nil))
+  [shape-wrapper nil])
 
 (defn face [side traits]
-  (apply dom/div #js {:className (str "face " (name side))}
-         (repeatedly (:number traits) (partial shape traits))))
+  (into [:div {:class (str "face " (name side))}]
+        (repeatedly (:number traits) #(shape traits))))
 
-(defn card [{:keys [traits selected drawn]} owner opts]
-  (reify
-    om/IRender
-    (render [this]
-      (let [faces (map (fn [[s t]] (face s t))
-                       (zipmap [:front :back]
-                               (remove nil? [(drawn traits) traits])))]
-        (dom/div #js {:className "card-container"}
-                 (apply dom/div #js {:className (class-names {"card"      true
-                                                              "blank"     (= :blank (:color traits))
-                                                              "selected"  (selected traits)
-                                                              "highlight" (:highlight traits)
-                                                              "flip"      (drawn traits)})
-                                     :onClick (fn [_] (put! (:card-selected opts) traits))
-                                     :onTouchEnd (fn [_] (put! (:card-selected opts) traits))}
-                        faces))))))
+(defn card [{:keys [traits selected drawn]}]
+  (let [faces (map (fn [[s t]] [face s t])
+                   (zipmap [:front :back]
+                           (remove nil? [(drawn traits) traits])))]
+    [:div {:class "card-container"}
+     (into [:div {:class (class-names {"card"      true
+                                       "blank"     (= :blank (:color traits))
+                                       "selected"  (selected traits)
+                                       "highlight" (:highlight traits)
+                                       "flip"      (drawn traits)})
+                  :on-click #(card-selected! traits)
+                  :on-touch-end #(card-selected! traits)}]
+           faces)]))
 
-(defn board [game owner opts]
-  (reify
-    om/IWillMount
-    (will-mount [_]
-      (go (loop []
-            (let [card (<! (:card-selected opts))]
-              (om/update! game (game/card-selected @game card)))
-              (when (game/over? @game)
-                (alert "We're done here."))
-            (recur))))
-    om/IRender
-    (render [this]
-      (apply dom/div #js {:className "board"}
-             (om/build-all card (map #(hash-map :traits %
-                                                :selected (:selected game)
-                                                :drawn (:drawn game))
-                                     (:board game))
-                           {:opts opts})))))
+(defn board []
+  (let [{:keys [board selected drawn]} @game-state]
+    (into [:div {:class "board"}]
+          (for [traits board]
+            ^{:key (hash traits)}
+            [card {:traits traits
+                   :selected selected
+                   :drawn drawn}]))))
 
-(defn game-ui [game owner opts]
-  (reify
-    om/IRender
-    (render [this]
-      (dom/div #js {:className "game"}
-               (dom/div #js {:className "controls"}
-                        (dom/p nil (str "Score: " (:score game)))
-                        (dom/button #js {:onClick (fn [_] (no-clumps! game))} "Clumpless?")
-                        (dom/button #js {:onClick (fn [_] (hint! game))} "Hint")
-                        (dom/button #js {:onClick (fn [_] (undo!))} "Undo")
-                        (dom/button #js {:onClick (fn [_] (new-game!))} "New Game"))
-               (om/build board game {:opts opts})))))
+(defn game-ui []
+  [:div {:class "game"}
+   [:div {:class "controls"}
+    [:p (str "Score: " (:score @game-state))]
+    [:button {:on-click no-clumps!} "Clumpless?"]
+    [:button {:on-click hint!} "Hint"]
+    [:button {:on-click undo!} "Undo"]
+    [:button {:on-click new-game!} "New Game"]]
+   [board]])
 
-(.initializeTouchEvents js/React true)
-(om/root game-ui
-         (new-game!)
-         {:target (. js/document (getElementById "app"))
-          :opts {:card-selected (chan)}})
+(defn init []
+  (when (empty? @game-state)
+    (new-game!))
+  (rdom/render [game-ui]
+               (.getElementById js/document "app")))
